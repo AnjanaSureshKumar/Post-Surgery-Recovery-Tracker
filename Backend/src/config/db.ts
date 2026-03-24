97a9514cc8b4f8dc10180e54e2da8def31876dd6
AnjanaSureshKumar <anjanasureshkumar18@gmail.com>
2026-03-24 11:55:30 +0530

    Delete Backend directory

--- Backend/src/config/db.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectToDatabase(mongoUri: string): Promise<void> {
-	if (!mongoUri) {
-		throw new Error("MONGO_URI is not defined");
-	}
-	mongoose.set("strictQuery", true);
-	await mongoose.connect(mongoUri);
-}
